1d094fa2aa367e57e6fb84876237a3c4ca904592
RayPerez22 <raynyc2285@gmail.com>
2022-08-31 15:57:02 -0400

    v3

--- src/services/todos-api.js
+++ src/services/todos-api.js
@@ -1,8 +1,12 @@
 // This file will be our endpoints
+// connecting to my express api
 import axios from 'axios'
+const baseURL = ''
+
 
 export const getTodos = () => {
     const URL = 'http://localhost:3001/todos'
+    // const URL = `${baseURL}/${id}`
     const response = axios.get(URL)
     return response
 }
